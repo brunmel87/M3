02bd0c522c9cdde858fcb1d3528edd9e840951fa
brunmel87 <b.melegavazza@gmail.com>
2022-06-04 00:42:15 -0300

    Seccond commit

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,2 +1,19 @@
+const commands = require('./commands/index.js');
 
+function print (output) {
+    process.stdout.write(output);
+    process.stdout.write('\nprompt > ');
+}
 
+// Output un prompt
+process.stdout.write('prompt > ');
+// El evento stdin 'data' se dispara cuando el user escribe una línea
+process.stdin.on('data', function (data) {
+  var args = data.toString().trim().split(' '); // remueve la nueva línea
+  var cmd = args.shift();
+  if(commands[cmd]) {
+    commands[cmd](args, print)  
+  } else {
+      print('Command not found');
+  };
+});
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,3 +1,44 @@
-module.exports = {
+var fs = require('fs');
+let request = require('request')
 
-}
+module.exports = {
+    pwd: function(args, print) {
+        print(process.cwd());
+    },
+    date: function(args, print) {
+        print(Date());
+    },
+    echo: function(args, print) {
+        print(args.join(' '));
+    },
+    ls: function(args, print) {
+        fs.readdir('.', function(err, files) {
+            if(err) throw err;
+            print(files.join('\n'))
+        })
+    },
+    cat: function(args, print) {
+        fs.readFile(args[0], 'utf8', function(err, data) {
+            if(err) throw err;
+            print(data);
+        })
+    },
+    head: function(args, print) {
+        fs.readFile(args[0], 'utf8', function(err, data){
+            if(err) throw err;
+            print(data.split('\n').splice(0, 10).join('\n'));
+        })
+    },
+    tail: function(args, print) {
+        fs.readFile(args[0], 'utf8', function(err, data){
+        if(err) throw err;
+        print(data.split('\n').splice(-10).join('\n'));
+        })
+    },
+    curl: function(args, print) {
+        request(args[0], function(err, data) {
+            if(err) throw err;
+            print(data.body);
+        })
+    }
+};
